e8e942a7f4897cba1447d8f6d434f0e6e1aabb4a
zg <zg@qq.com>
2022-08-21 22:28:05 +0800

    fix add coupon

--- pages/member/payDetails/index.js
+++ pages/member/payDetails/index.js
@@ -5,7 +5,67 @@ Page({
    */
   data: {
     vipMonth: true,
-    showRule: true,
+    showRule: false,
+    showCoupon: false,
+    activeCouponId: 2,
+    buttons: [
+      {
+        type: 'default',
+        className: '',
+        text: '辅助操作',
+        value: 0
+    },
+      {
+        type: "primary",
+        className: "",
+        text: "主操作",
+        value: 1,
+      },
+    ],
+    couponList: [
+      {
+        title: "券名称111",
+        time: "2022.08.08-2022.10.10",
+        id: 1,
+        price: 2,
+      },
+      {
+        title: "券名称222",
+        time: "2022.08.08-2022.10.10",
+        id: 2,
+        price: 1,
+      },
+      {
+        title: "上门寄件券",
+        time: "2022.08.08-2022.10.10",
+        id: 3,
+        price: 2,
+      },
+      {
+        title: "券名称441",
+        time: "2022.08.08-2022.10.10",
+        id: 4,
+        price: 2,
+      },
+      {
+        title: "券名称111",
+        time: "2022.08.08-2022.10.10",
+        id: 5,
+        price: 2,
+      },
+      {
+        title: "券名称222",
+        time: "2022.08.08-2022.10.10",
+        id: 6,
+        price: 2,
+      },
+      {
+        title: "上门寄件券",
+        time: "2022.08.08-2022.10.10",
+        id: 7,
+        price: 2,
+      },
+    ],
   },
 
   /**
@@ -47,6 +107,21 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {},
+
+  showCouponHandle() {
+    this.setData({
+      showCoupon: true,
+    });
+  },
+
+  couponChange(e) {
+    let { item } = e.currentTarget.dataset;
+    console.log(item.id);
+    this.setData({
+      activeCouponId: item.id,
+    });
+  },
+  diaCouponClick(e) {},
 
   vipMonthHandle() {
     this.setData({
